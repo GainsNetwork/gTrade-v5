4954ee202365981153ddc0426a5e7842eab534a3
GainsNetwork <contact@gains.trade>
2022-03-07 10:42:14 +0100

    added interfaces

---
+++ contracts/interfaces/VaultInterfaceV5.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface VaultInterfaceV5{
+	function sendDaiToTrader(address, uint) external;
+	function receiveDaiFromTrader(address, uint, uint) external;
+	function currentBalanceDai() external view returns(uint);
+}
